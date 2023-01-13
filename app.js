8e461ad0a867ebb5beb017b7465100a9cbf63fc6
Flavien <flavienrakotoarison8@gmail.com>
2023-01-13 16:51:03 +0300

    use file conf

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const routerUtilisateurs = require("./src/routers/utilisateur-routes");
 const routerVoitures = require("./src/routers/voiture-routes");
 const routerObject = require("./src/routers/object-routes");
 const routerReparation = require("./src/routers/reparation-routes");
+const config = require('config');
+
 var cors = require('cors');
 
 var corsOptions = {
@@ -24,14 +26,16 @@ app.use("/api/v1", routerVoitures);
 app.use("/api/v1", routerObject);
 app.use("/api/v1", routerReparation);
 
+const port = config.get('port');
+const dbUrl = config.get('database.url');
 
-connect("mongodb://127.0.0.1:27017/", (err) => {
+connect(dbUrl, (err) => {
   if (err) {
     console.log("Erreur lors de la connexion à la base de données");
     process.exit(-1);
   } else {
     console.log("Connexion avec la base de données établie");
-    app.listen(3000);
-    console.log("Attente des requêtes au port 3OOO");
+    app.listen(port);
+    console.log("Attente des requêtes au port "+port);
   }
 }); 
--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,8 @@
 const { MongoClient, Db } = require("mongodb");
+const config = require('config');
 
 let client = null;
+const dbName = config.get('database.name');
 
 function connect(url, callback) {
   if (client === null) {
@@ -20,7 +22,7 @@ function connect(url, callback) {
 }
 
 function db() {
-  var db = new Db(client, "garage_mean");
+  var db = new Db(client, dbName);
   return db;
 }
 
