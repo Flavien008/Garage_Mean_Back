db479fd544ecf80c2a5a23143d6888f3ae618af7
Flavien <flavienrakotoarison8@gmail.com>
2023-01-26 14:38:48 +0300

    statistic api

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,37 +1,85 @@
 const { ObjectID } = require("bson");
 const client = require("../db/connect");
 
-const benefice= async (req, res) => {
+const benefice = async (req, res) => {
     try {
-        const data= req.body;
-        console.log(data);
-        const startDate = new Date(new Date(data.debut).toISOString().slice(0, 10));
-        const endDate = new Date(new Date(data.fin).toISOString().slice(0, 10));
-        var entree = 0;
-        var sortie = 0;
-        var condition;
+        // Connect to the MongoDB server
+        const data = req.body;
+        const startDate = new Date(data.debut);
+        startDate.setUTCHours(0, 0, 0, 0);
+        const endDate = new Date(data.fin);
+        endDate.setUTCHours(23, 59, 59, 999);
+        const collection = client.db().collection('journal');
 
-        if(startDate != endDate) condition= {$gte:startDate,$lte:endDate}
-        console.log(condition);
-
-        const collection = client.db().collection("journal");
-        var query ={ date: condition };
-        collection.find(query).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            for( var i = 0 ; i < result.length ; i++){
-                entree += Number(result[i].entree);
-                sortie += Number(result[i].sortie);
+        const pipeline = [
+            {
+              $match: {
+                date: {
+                  $gte: startDate,
+                  $lte: endDate
+                }
+              }
+            },
+            {
+              $group: {
+                _id: null,
+                entree: { $sum: '$entree' },
+                sortie: { $sum: '$sortie' },
+                benefice: { $sum: { $subtract: ["$entree", "$sortie"] } }
+              }
             }
-            res.status(200).json({ entree : entree, sortie : sortie ,  benefice : (entree-sortie) });
-        });
+          ];
 
+        // Execute the aggregate pipeline
+        const result = await collection.aggregate(pipeline).toArray();
+        res.status(200).json(result);
     } catch (error) {
         console.log(error);
         res.status(501).json(error);
     } 
 };
 
+
+const statistique = async (req, res) => {
+    try {
+        // Connect to the MongoDB server
+        const data = req.body;
+        const startDate = new Date(data.debut);
+        startDate.setUTCHours(0, 0, 0, 0);
+        const endDate = new Date(data.fin);
+        endDate.setUTCHours(23, 59, 59, 999);
+        const collection = client.db().collection('journal');
+
+        const pipeline = [
+            {
+                $match: {
+                    date: {
+                        $gte: startDate,
+                        $lte: endDate
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+                    entree: { $sum: '$entree' },
+                    sortie: { $sum: '$sortie' },
+                    benefice: { $sum: { $subtract: ["$entree", "$sortie"] } }
+                }
+            }
+        ];
+
+        // Execute the aggregate pipeline
+        const result = await collection.aggregate(pipeline).toArray();
+        res.status(200).json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(501).json(error);
+    } 
+};
+
+
 module.exports = {
-    benefice
+    benefice,
+    statistique
  };
--- src/routers/dashboard-routes.js
+++ src/routers/dashboard-routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const auth = require('../middleware/auth');
 const {
-  benefice
+  benefice,statistique
 } = require("../controllers/dashboard");
 const router = express.Router();
 
 router.route("/benefice").post(benefice);
+router.route("/statistic").post(statistique);
+
 
 
 module.exports = router;
